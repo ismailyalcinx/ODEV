7837029ab06bc9a4a208e2ab3875fdb90f4806ec
ismailyalcinx <ismailyalcinx315@gmail.com>
2026-04-09 10:47:44 +0300

    RAZOR 3

---
+++ RAZOR 3 ÖDEV/Privacy.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        [BindProperty]
+        public int Yas { get; set; }
+
+        public string Mesaj { get; set; }
+
+        public void OnGet()
+        {
+        }
+
+        public void OnPost()
+        {
+            if (Yas < 0)
+            {
+                Mesaj = "Hata: Yaş 0'dan küçük olamaz!";
+            }
+            else if (Yas >= 18)
+            {
+                Mesaj = "Reşitsiniz";
+            }
+            else
+            {
+                Mesaj = "Reşit değilsiniz";
+            }
+        }
+    }
+}
