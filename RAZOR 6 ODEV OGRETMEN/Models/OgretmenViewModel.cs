06ca9900986944731966091a0eb493577bc49561
ismailyalcinx <ismailyalcinx315@gmail.com>
2026-04-09 10:28:23 +0300

    Razor 6 ogretmen

---
+++ RAZOR 6 ODEV OGRETMEN/Models/OgretmenViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace RAZOR6OGRETMEN.Models
+{
+    public class OgretmenViewModel
+    {
+        public string Ad { get; set; }
+        public string Brans { get; set; }
+        public double Maas { get; set; }
+        public double ZamliMaas => Maas * 1.10;
+    }
+}
