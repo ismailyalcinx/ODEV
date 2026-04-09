9fd67c73c7c5a0941dadcee2f4a69832a6537f39
ismailyalcinx <ismailyalcinx315@gmail.com>
2026-04-09 10:26:54 +0300

    Add files via upload

---
+++ Razor 6 Yas Ortalaması/RAZOR 6 ODEV OGRETMEN/Models/Ogretmen.cs
@@ -0,0 +1,9 @@
+﻿namespace RRAZOR6OGRETMEN.Models
+{
+    public class Ogretmen
+    {
+        public string Ad { get; set; }
+        public string Brans { get; set; }
+        public double Maas { get; set; }
+    }
+}
---
+++ Razor 6 Yas Ortalaması/RAZOR 6 ODEV OGRETMEN/Privacy.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RAZOR6OGRETMEN.Models;
+
+namespace RAZOR6OGRETMEN.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public List<OgretmenViewModel> Ogretmenler { get; set; }
+
+        public void OnGet()
+        {
+            Ogretmenler = new List<OgretmenViewModel>
+            {
+                new OgretmenViewModel { Ad = "Ahmet Yılmaz", Brans = "Bilişim", Maas = 30000 },
+                new OgretmenViewModel { Ad = "Ayşe Demir", Brans = "Matematik", Maas = 28000 },
+                new OgretmenViewModel { Ad = "Mehmet Kaya", Brans = "Fizik", Maas = 29000 }
+            };
+        }
+    }
+}
