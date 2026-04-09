05319a24ae0dc4baa35549e173d552e5ae445d02
ismailyalcinx <ismailyalcinx315@gmail.com>
2026-04-09 10:02:16 +0300

    Add files via upload

---
+++ Razor 6 Yas Ortalaması/Privacy.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using RAZOR6YASORTALAMASI.Models;
+
+namespace RAZOR6YASORTALAMASI.Pages
+{
+    public class test1Model : PageModel
+    {
+        public Student Ogrenci { get; set; }
+
+        public void OnGet()
+        {
+            Ogrenci = new Student
+            {
+                Ad = "ismail",
+                Yas = 18,
+                Ortalama = 85.5
+            };
+        }
+    }
+}
