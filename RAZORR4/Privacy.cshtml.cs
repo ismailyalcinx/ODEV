89066824727d6a14a939360d1c20854817904ccb
ismailyalcinx <ismailyalcinx315@gmail.com>
2026-04-17 08:37:12 +0300

    Add files via upload

---
+++ RAZORR4/Privacy.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages
+{
+    public class test1Model : PageModel
+    {
+        [BindProperty]
+        public bool GirisYaptiMi { get; set; }
+
+        public void OnGet()
+        {
+        }
+
+        public void OnPost(bool durum)
+        {
+            GirisYaptiMi = durum;
+        }
+    }
+}
