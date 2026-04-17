fb993bdae3e71253e2ea0c85110ec12333290840
ismailyalcinx <ismailyalcinx315@gmail.com>
2026-04-17 09:18:06 +0300

    Add files via upload

---
+++ RAQZOR4+sınıfiçietk/Privacy.cshtml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+
+namespace proje11.Pages
+{
+    public class test1Model : PageModel
+    {
+        public List<int> Sayilar { get; set; }
+
+        public void OnGet()
+        {
+            Sayilar = new List<int> { 10, 15, 22, 33, 40 };
+        }
+    }
+}
