d54e0dde436ac0c49864d1cebfb38dd9284033b8
ismailyalcinx <ismailyalcinx315@gmail.com>
2026-04-17 09:36:24 +0300

    Add files via upload

---
+++ razor4+derssonu/Privacy.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+
+namespace proje11.Pages
+{
+    public class test1Model : PageModel
+    {
+        public List<string> Ogrenciler { get; set; }
+
+        public void OnGet()
+        {
+            Ogrenciler = new List<string>
+            {
+                "Ali",
+                "Ayşe",
+                "Fatma",
+                "Mehmet",
+                "Veli"
+            };
+        }
+    }
+}
