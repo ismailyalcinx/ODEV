e76f9eb97a5460ae64fe7dd2a0b933384a577aea
ismailyalcinx <ismailyalcinx315@gmail.com>
2026-04-30 08:52:21 +0300

    Add files via upload

---
+++ RAZOR7 VALİDATİON/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RAZOR7.Models
+{
+    public class Student
+    {
+        public string Ad { get; set; }
+        public int Yas { get; set; }
+        public double Ortalama { get; set; }
+
+        [EmailAddress(ErrorMessage = "Geçerli email gir")]
+        public string Email { get; set; }
+    }
+}
---
+++ RAZOR7 VALİDATİON/Privacy.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using RAZOR7.Models; // Kırmızı çizgiyi bitiren sihirli satır!
+
+namespace RAZOR7.Pages
+{
+    public class test1Model : PageModel
+    {
+        public Student Ogrenci { get; set; }
+
+        public void OnGet()
+        {
+            Ogrenci = new Student
+            {
+                Ad = "Ali",
+                Yas = 18,
+                Ortalama = 90.0,
+                Email = "ali@rastgele.com"
+            };
+        }
+    }
+}
